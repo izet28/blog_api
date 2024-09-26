950701fef027887c4f3546b9375c0a5b2c0f3469
izet28 <zetcoin29@gmail.com>
2024-09-26 17:50:21 +0700

    fix update tag

--- services/blog_service.go
+++ services/blog_service.go
@@ -3,6 +3,7 @@ package services
 import (
 	"blog/config"
 	"blog/models"
+	"fmt"
 )
 
 // GetAllBlogs - Mendapatkan semua blog dari database
@@ -49,19 +50,40 @@ func GetBlogByID(id uint) (*models.Blog, error) {
 // UpdateBlog - Memperbarui blog berdasarkan ID
 func UpdateBlog(id uint, updatedBlog models.Blog) (*models.Blog, error) {
 	var blog models.Blog
-	if err := config.DB.First(&blog, id).Error; err != nil {
+	if err := config.DB.Preload("Tags").First(&blog, id).Error; err != nil {
 		return nil, err
 	}
-	// Perbarui data blog
+
+	// Hapus relasi lama di tabel penghubung (blog_tags)
+	if err := config.DB.Model(&blog).Association("Tags").Clear(); err != nil {
+		return nil, fmt.Errorf("failed to clear old tags: %v", err)
+	}
+
+	// Memeriksa dan membuat atau menemukan tag yang sesuai
+	for i, tag := range updatedBlog.Tags {
+		if tag.ID == 0 {
+			if err := config.DB.FirstOrCreate(&updatedBlog.Tags[i], models.Tag{Name: tag.Name}).Error; err != nil {
+				return nil, fmt.Errorf("failed to create or find tag: %s", tag.Name)
+			}
+		}
+	}
+
+	// Memperbarui data blog kecuali relasi tags
 	blog.Title = updatedBlog.Title
 	blog.Content = updatedBlog.Content
 	blog.CategoryID = updatedBlog.CategoryID
 	blog.SubCategoryID = updatedBlog.SubCategoryID
-	blog.Tags = updatedBlog.Tags
 
+	// Simpan perubahan pada blog (kecuali tags)
 	if err := config.DB.Save(&blog).Error; err != nil {
 		return nil, err
 	}
+
+	// Tambahkan relasi tags baru
+	if err := config.DB.Model(&blog).Association("Tags").Replace(updatedBlog.Tags); err != nil {
+		return nil, fmt.Errorf("failed to update tags: %v", err)
+	}
+
 	return &blog, nil
 }
 
